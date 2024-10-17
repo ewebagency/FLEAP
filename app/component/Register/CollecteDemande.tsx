0607590d89182423843a88b2130a7fd73a03ab9a
ArthurPouzCS <arthur.pouzargue@student-cs.fr>
2024-10-17 16:53:45 +0200

    1st test

--- app/component/Register/CollecteDemande.tsx
+++ app/component/Register/CollecteDemande.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const CollecteDemande = () => {
 
     return (
-        <div className="flex justify-between items-center bg-green-600 rounded-xl px-2 mx-1 duration-300 hover:px-3 hover:py-1 click:scale-90">
+        <div className="flex justify-between items-center bg-green-600 rounded-xl px-2 mx-1 duration-300 hover:px-3 hover:py-1">
             <div className="text-white mr-2 mb-1">🚚</div>
             <div className="text-white font-thin text-xs">Demander une collecte</div>
         </div>
