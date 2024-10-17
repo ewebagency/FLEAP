dd9f2ef959b76edd14b62c0471f2c051ffb837a1
rogalio <rolf.mouchelblaisot@gmail.com>
2024-10-17 18:14:20 +0200

    auth fixed

---
+++ app/api/auth/login/route.tsx
@@ -0,0 +1,10 @@
+// app/api/auth/login/route.ts
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const clientId = process.env.NEXT_PUBLIC_TRACKDECHETS_CLIENT_ID;
+  const redirectUri = encodeURIComponent(`${process.env.NEXT_PUBLIC_APP_URL}/api/auth/callback`);
+  const authUrl = `https://app.trackdechets.beta.gouv.fr/oauth2/authorize/dialog?response_type=code&redirect_uri=${redirectUri}&client_id=${clientId}`;
+
+  return NextResponse.redirect(authUrl);
+}
