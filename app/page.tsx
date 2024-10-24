c0fa3982bcc3135be664b50a87a917fd7c3121f2
ArthurPouzCS <arthur.pouzargue@student-cs.fr>
2024-10-24 16:24:03 +0200

    apostrophe fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-green-800">Bienvenue chez FLEAP</h1>
       <div className="mt-10 bg-green-800 rounded-lg shadow-lg p-6 max-w-sm w-full text-center">
-        <div className="mt-4 text-lg text-white">Vous n'êtes pas connecté</div>
+        <div className="mt-4 text-lg text-white">Vous n&apos;êtes pas connecté</div>
         <a href="/auth/signin" className="mt-4 btn btn-primary w-full bg-green-600 hover:bg-green-500 text-white text-lg border-white hover:border-white">Se connecter</a>
         <div className="mt-6 flex justify-between items-center">
-          <div className="text-sm text-gray-200">Vous n'avez pas de compte ?</div>
+          <div className="text-sm text-gray-200">Vous n&apos;avez pas de compte ?</div>
           <a href="/auth/signup" className="btn btn-primary text-xs bg-green-600 hover:bg-green-500 text-white border-white hover:border-white">S'inscrire</a>
         </div>
       </div>
