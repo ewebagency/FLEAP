0aa953551fde0a31c322b886cf29f184b45b8ec7
ArthurPouzCS <arthur.pouzargue@student-cs.fr>
2024-11-05 17:41:36 +0100

    fix z-50 error assistance

--- app/component/Assistance.tsx
+++ app/component/Assistance.tsx
@@ -6,8 +6,8 @@ export default function Assistance() {
         <div className="flex items-center justify-start">
             <div onClick={() => setOpen(!open)} className="text-md font-bold text-gray-400 mt-2 bg-gray-200 cursor-pointer rounded-lg px-2 py-1">📞 Assistance</div>
             {open && 
-            <div className="fixed inset-0 bg-black/50 flex items-center justify-center" onClick={() => setOpen(false)}>
-                <div className="bg-white p-6 rounded-lg shadow-lg max-w-md text-center">
+            <div className="fixed inset-0 z-50 bg-black/50 flex items-center justify-center" onClick={() => setOpen(false)}>
+                <div className="bg-white p-6 rounded-lg shadow-lg max-w-md text-center" onClick={e => e.stopPropagation()}>
                     <h2 className="text-2xl font-bold text-gray-800">Auguste Sohm</h2>
                     <p className="text-gray-600 mt-2">Service client</p>
                     <h3 className="text-lg font-semibold text-gray-700 mt-4">Contactez-nous :</h3>
