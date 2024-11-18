dfc93cc94db82e138318447bb9974b42ba61eb41
ArthurPouzCS <arthur.pouzargue@student-cs.fr>
2024-11-18 12:07:00 +0100

    add log to understand

--- app/api/demande_collecte/web_hook/get_webhooks/route.ts
+++ app/api/demande_collecte/web_hook/get_webhooks/route.ts
@@ -264,7 +264,7 @@ const createWebHook = async (token:string, id_company:string, uri:string) => {
         }*/
 
         console.log('WebHook créé', response.data);
-        return {status: 200};
+        return {status: 200, data: response.data};
     } catch (error) {
         console.error("Erreur complète:", error);
         return {status: 500};
