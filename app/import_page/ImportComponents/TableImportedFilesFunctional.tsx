38c724288243c71749a83ce3653a81695a46ee7c
ArthurPouzCS <arthur.pouzargue@student-cs.fr>
2025-09-10 13:37:31 +0200

    filtre site un par un - filiere nom dans registre - pdf clickable - numeroBon display michel sas v2

--- app/import_page/ImportComponents/TableImportedFilesFunctional.tsx
+++ app/import_page/ImportComponents/TableImportedFilesFunctional.tsx
@@ -9,7 +9,7 @@ import { toast } from 'react-hot-toast';
 import { cofounders_user_id } from '@/app/component/SideBar';
 import CofounderStatusFilter from './CofounderStatusFilter';
 import ExtractBonProcessor from './JobProcessor/ExtractBonProcessor';
-import LoopStarterButton from './ExtractMetaDoc/components/LoopStarterButton';
+//import LoopStarterButton from './ExtractMetaDoc/components/LoopStarterButton';
 
 
 const TableImportedFilesFunctional: React.FC = () => {
@@ -176,7 +176,7 @@ const TableImportedFilesFunctional: React.FC = () => {
                     {cofounders_user_id(user_id) &&  
                         <div className="flex items-center justify-between">
                             {/*<ExtractBonProcessor />*/}
-                            <LoopStarterButton/>
+                            {/*<LoopStarterButton/>*/}
                         </div>                  
                         
                     }            
