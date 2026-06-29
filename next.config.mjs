b06f972a2c775d1acc8081313d84f16dbd2995e9
EWEB Agency <ewebagency@sdisolution.fr>
2026-06-29 14:31:25 +0200

    test(staging): correctif propre du build (retire ignoreBuildErrors)

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    typescript: {
-        ignoreBuildErrors: true,
-    },
     images: {
         domains: [process.env.NEXT_PUBLIC_SUPABASE_URL?.replace('https://', '')]
     },
